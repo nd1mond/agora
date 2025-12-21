17188859cf8bd2f31c0be40afc0a9f533327b8f4
nd1mond <za1tsef19@gmail.com>
2025-12-21 18:36:51 +0300

    Отправка письма.

--- backend/main.py
+++ backend/main.py
@@ -43,10 +43,10 @@ conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_FROM=os.getenv("MAIL_FROM"),
-    MAIL_PORT=int(os.getenv("MAIL_PORT", 465)),  # Порт должен быть числом
+    MAIL_PORT=int(os.getenv("MAIL_PORT", 587)),  # Порт должен быть числом
     MAIL_SERVER=os.getenv("MAIL_SERVER"),
-    MAIL_STARTTLS=False,
-    MAIL_SSL_TLS=True,
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True
 )
